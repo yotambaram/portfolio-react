e28496c6c5c22531266af3a58be40a49cfb3c33d
yotamba12 <yotambaram@gmail.com>
2020-05-29 23:34:25 -0700

    cmm margin center and about me content update

--- src/components/id/index.js
+++ src/components/id/index.js
@@ -3,7 +3,6 @@ import "./style.css"
 
 export default function Id() {
   return (
-    <div className="col-12 col-md-3 border-dark" id="id-card-holder">
       <div className='card border-dark mb-3 shadow mb-5 bg-white rounded img-thumbnail' id='id-card'>
         <img src={require('../../public/jpg/yotam2.jpg')} className='card-img-top border' alt='profilepic' id="" value="propic" />
         <div className="card-body">
@@ -15,6 +14,5 @@ export default function Id() {
           </div>
         </div>
       </div>
-    </div>
   )
 }
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -8,7 +8,7 @@ export default function HomePage() {
     <div id="home">
       <div className='row'>
         <div className='col-12 col-md-12 border-dark backgraund-test' id='home-cover-color'>
-          <img src={require('../../public/jpg/yotam2.jpg')} alt="profilepic" id='home-profile-pic' className='img-thumbnail position-absolute' value="propic"/>
+          <img src={require('../../public/jpg/yotam2.jpg')} alt="profilepic" id='home-profile-pic' className='img-thumbnail position-absolute' value="propic" />
         </div>
       </div>
       <div className='row'>
@@ -18,23 +18,32 @@ export default function HomePage() {
             <h1>Yotam Baram</h1>
           </div>
           <div>
-            <h3>Full Stack Developer</h3><br /><br />
-            <a>My name is and i'm from and you will know more after the career services will tell me what can i tell you
-              and what not</a>
-            <a>My main goal here is to put together a collection of clichés but nicely and neatly without writing about
-              a wise man who once told<br /> me that a little bit of text is not good and a lot of it is not good so
-              everything in life is in the middle and I just wanted to fill<br /> in lines here So I wrote a little
-              nonsense to see what was happening.
+            <h3>Full Stack Developer</h3><br />
+            <a><a className="h5">ABOUT ME</a><br />
+            10 years experience in broadcast and commercial production as an executive producer and content editor for various tv channels. Pre and post production; live remote production.
+            I’ve played key roles in creating high exposure content and producing major events.
+            </a><br />
+            <a>During March 2019 I’ve relocated from Israel to Seattle,     WA, due to my wife’s relocation job. 
+              I took advantage of this major change as an opportunity to learn software development and to route myself into a career change which I was hoping for a long back. 
+              Since our move, I’ve been dedicated and targeted to gain knowledge, learning and developing my skills as a full stack developer. I’ve learnt independently and also completed successfully formal crossovers (certificates).  
+              Now I’m ready to take it to the next level and leverage the knowledge and experience I gained at my next role. 
+            </a><br /><br />
+            <a><a className="h5">CAREER PATH</a><br />
+              MX1 Satellite services, Television playout operator, 2018-2019
+              Entertainment Channel, Senior News Content Editor, 2014-2017
+              The Israeli Sports Channel, Executive Producer and Content Editor,  2009-2014
+            </a><br /><br />
+            <a><a className="h5">PERSONAL LIFE</a><br />
+              From Israel, relocated to US with my family in march 2019.
+              Married to Ayelet and father to Adam (5) and Eleanor (2).
+              We love to spend family time together, travel around mainly in nature.
+            </a><br /><br /> 
+              <a><a className="h5">PERSONAL LIFE</a><br />
+              Planning & driving schedule
+              Results-oriented go-getter
+              Partner management & team work 
+              Brainstorming and innovation
             </a>
-            <a>The applicant must grow, but not grow too much because everything in life is in the middle.</a>
-            <a>Thanks to Google translate that I haven't even tested and probably the logical difference between
-              languages has created something special here.<br /> One-time.
-            </a>
-            <a>If you've called here, it's not too late to stop and stop if it's right away.
-              It is advisable to read everything upside down now.<br /> At the end you will find that Paul is still
-              alive.
-            </a>
-            <a></a>
           </div>
         </div>
       </div>
